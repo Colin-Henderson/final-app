65a9fb7cca6a69f236b07c6f66a9a1158dd060ee
Colin Henderson <colinhenderson@Colins-MacBook-Pro.local>
2018-03-31 11:15:55 -0400

    Added Favicon to browser tab

---
+++ final-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Nav from "./components/Nav";
+
+const App = () => (
+ <Router>
+     <Nav />
+</Router>
+);
+
+export default App;
